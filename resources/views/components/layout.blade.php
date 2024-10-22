ca8c02d54b31ba964a76847d1b72e787744027d4
Zoruasy <1076302@hr.nl>
2024-10-22 22:29:05 +0200

    Homepage gemaakt, x-layout gemaakt, html- structuur verbeterd, login en logout gemaakt, ook wanneer je inlogd krijg je bepaalde dingen te zien

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,21 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-{{ $slot }}
-<nav>
-    <a href="/">home</a>
-    <a href="/catalog">catalog</a>
-</nav>
+<!-- resources/views/components/layout.blade.php -->
+<div class="container">
 
 
+    <!-- Toon iets voor ingelogde gebruikers -->
+    @auth
+        <p>Hallo, {{ Auth::user()->name }}! Bedankt voor het inloggen.</p>
+        <p>Hier is exclusieve content alleen voor ingelogde gebruikers.</p>
+    @endauth
 
-</body>
-</html>
+    <!-- Toon iets voor niet-ingelogde gebruikers -->
+    @guest
+
+        <a href="{{ route('login') }}">Inloggen</a>
+        <a href="{{ route('register') }}">Registreren</a>
+        <a href="/">Home</a>
+        <a href="/catalog">Catalogus</a>
+    @endguest
+
+    {{ $slot }}  <!-- Dit toont de inhoud die in de component is ingevoegd -->
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
+<!-- resources/views/home.blade.php -->
 <x-layout>
-    <h1> Welcome to the zoo!</h1>
-
+    <h1>Welcome to the zoo!</h1>
+    <p>Dit is de inhoud van de homepage. Hier kun je meer informatie over de dierentuin vinden.</p>
 </x-layout>
